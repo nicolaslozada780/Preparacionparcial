5f596f64f9b60546828f95c7dbd41d66f1f46bba
nicolas <a2018115260@estudiantes.upsa.edu.bo>
2021-03-13 12:22:48 -0400

    controlador creado

---
+++ EvaNicolasLozada/ApiCountry/Models/Datacontext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ApiCountry.Models
+{
+    public class Datacontext:DbContext
+    {
+        public Datacontext():base("defaultConnection")
+        {
+
+        }
+    }
+}
